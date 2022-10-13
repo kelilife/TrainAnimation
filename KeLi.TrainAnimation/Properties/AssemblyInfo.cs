7a51c6fc5d3be9bafddc35ed7a53cd00ec151bd6
KeLi <kelilive@outlook.com>
2022-10-14 00:05:31 +0800

    Update

--- KeLi.TrainAnimation/Properties/AssemblyInfo.cs
+++ KeLi.TrainAnimation/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeLi.TrainAnimation")]
+[assembly: AssemblyTitle("TrainAnimation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KeLi.TrainAnimation")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("KeLi")]
+[assembly: AssemblyProduct("TrainAnimation")]
+[assembly: AssemblyCopyright("Copyright © KeLi 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
